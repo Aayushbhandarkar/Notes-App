2e6b572e2c95036199f94bb6a54d084405d9fe54
Ayush Bhandarkar <163156376+Aayushbhandarkar@users.noreply.github.com>
2025-09-02 18:29:47 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://notes-app-backend-tfun.onrender.com';
+const API_URL = import.meta.env.VITE_API_URL || 'https://notes-app-backend1-ucg9.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
